384d0f468e6631ec00435b90f7ba2e5ea4d8e68f
coder-nmn <coder.nmn@gmail.com>
2026-05-30 03:54:53 +0530

    104

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -8,7 +8,7 @@ const imagekit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 })
 
-
+// create post controller
 async function createPostController(req, res) {
     // console.log(req.body, req.file);
 
@@ -62,6 +62,89 @@ async function createPostController(req, res) {
 
 }
 
+//get post controller
+async function getPostController(req, res){
+    
+    const token = req.cookies.token;
+    let decoded;
+    try {
+        
+     decoded = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message : "token invalid"
+        })
+        
+    }
+
+    const userId = decoded.id;
+
+    const posts = await postModel.find({
+        user : userId
+    })
+
+    res.status(200).json({
+        message : "post fetched finely",
+        posts
+    })
+
+}
+
+
+// get post detail
+async function getPostDetails(req, res){
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message : "token not found, unauthorised"
+        })
+    }
+
+    let decoded = null;
+    try {
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+    } catch (error) {
+        res.status(401).json({
+            message  : " token wrong"
+        })
+        
+    }
+
+    const userId = decoded.id
+    const postId = req.params.postId
+
+
+    const post  = await postModel.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message : "post not founs"
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+    console.log(post.user.toString());
+    console.log(userId);
+    
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message  : "forbidden content"
+        })
+    }
+
+    return res.status(200).json({
+        message : "post fetched finely",
+        post
+    })
+
+    
+
+}
 module.exports = {
-    createPostController
+    createPostController,
+    getPostController,
+    getPostDetails
 }
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -9,5 +9,14 @@ const upload = multer({storage  :multer.memoryStorage() })
 //POST /api/post
 postRouter.post("/",upload.single("img"),  postController.createPostController)
 
+//GET /api/posts
+postRouter.get("/", postController.getPostController )
+
+
+//GET /api/posts/details/:postid
+//return an detail about specific post with the id, also check whther the post belongs to the user that thee request comes from
+
+postRouter.get("/details/:postId", postController.getPostDetails)
+
 
 module.exports = postRouter
